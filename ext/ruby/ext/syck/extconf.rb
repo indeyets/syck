6ce6d4c638c43f56bf3ccc3c910e9615f64bb442
_why <why@whytheluckystiff.net>
2003-05-07 22:02:37 +0000

    Extension now builds with install.rb and the rest of the YAML.rb dist.

--- ext/ruby/ext/syck/extconf.rb
+++ ext/ruby/ext/syck/extconf.rb
@@ -2,7 +2,7 @@ require 'mkmf'
 
 [ 'gram.c', 'gram.h', 'handler.c', 'node.c', 'syck.c', 'syck.h', 'token.c', 'implicit.c' ].
     each { |codefile|
-      `cp ../../../lib/#{codefile} #{codefile}`
+      `cp #{File::dirname $0}/../../../../lib/#{codefile} #{codefile}`
   }
 have_header( "st.h" )
 create_makefile( "syck" )
