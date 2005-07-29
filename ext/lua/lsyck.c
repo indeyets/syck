7c3ca1e31bd79ec5063c41a6eabe760771a35b06
Zachary P. Landau <kapheine@divineinvasion.net>
2005-07-29 02:04:15 +0000

    Initial import of Lua extension

---
+++ ext/lua/lsyck.c
@@ -0,0 +1,247 @@
+/*
+ * lsyck.c
+ *
+ * $Author$
+ * $Date$
+ *
+ * Copyright (C) 2005 Zachary P. Landau <kapheine@divineinvasion.net>
+ */
+
+#include <syck.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "lauxlib.h"
+#include "lua.h"
+#include "lualib.h"
+
+//static lua_State *bonus->L;
+
+struct emitter_xtra {
+	lua_State *L;
+	luaL_Buffer output;
+};
+
+struct parser_xtra {
+	lua_State *L;
+};
+
+
+SYMID
+lua_syck_parser_handler(SyckParser *p, SyckNode *n)
+{
+	struct parser_xtra *bonus = (struct parser_xtra *)p->bonus;
+	int o, o2, o3 = -1;
+	SYMID oid;
+	int i;
+
+	switch (n->kind) {
+		case syck_str_kind:
+			if (n->type_id == NULL || strcmp(n->type_id, "str") == 0) {
+				lua_pushlstring(bonus->L, n->data.str->ptr, n->data.str->len);
+				o = lua_gettop(bonus->L);
+			}
+			else if (strcmp(n->type_id, "null") == 0)
+			{
+				lua_pushnil(bonus->L);
+				o = lua_gettop(bonus->L);
+			}
+			else if (strcmp(n->type_id, "bool#yes") == 0)
+			{
+				lua_pushboolean(bonus->L, 1);
+				o = lua_gettop(bonus->L);
+			}
+			else if (strcmp(n->type_id, "bool#no") == 0)
+			{
+				lua_pushboolean(bonus->L, 0);
+				o = lua_gettop(bonus->L);
+			}
+			else if (strcmp(n->type_id, "int#hex") == 0)
+			{
+				long intVal = strtol(n->data.str->ptr, NULL, 16);
+				lua_pushnumber(bonus->L, intVal);
+				o = lua_gettop(bonus->L);
+			}
+			else if (strcmp(n->type_id, "int") == 0)
+			{
+				long intVal = strtol(n->data.str->ptr, NULL, 10);
+				lua_pushnumber(bonus->L, intVal);
+				o = lua_gettop(bonus->L);
+			}
+			else
+			{
+				lua_pushlstring(bonus->L, n->data.str->ptr, n->data.str->len);
+				o = lua_gettop(bonus->L);
+			}
+			break;
+
+		case syck_seq_kind:
+			lua_newtable(bonus->L);
+			o = lua_gettop(bonus->L);
+			for ( i=0; i < n->data.list->idx; i++ )
+			{
+				oid = syck_seq_read(n, i);
+				syck_lookup_sym(p, oid, (char **)&o2);
+				lua_pushvalue(bonus->L, o2);
+				lua_rawseti(bonus->L, o, i+1);
+			}
+			break;
+
+		case syck_map_kind:
+			lua_newtable(bonus->L);
+			o = lua_gettop(bonus->L);
+			for ( i=0; i < n->data.pairs->idx; i++ )
+			{
+				oid = syck_map_read(n, map_key, i);
+				syck_lookup_sym(p, oid, (char **)&o2);
+				oid = syck_map_read(n, map_value, i);
+				syck_lookup_sym(p, oid, (char **)&o3);
+
+				lua_pushvalue(bonus->L, o2);
+				lua_pushvalue(bonus->L, o3);
+				lua_settable(bonus->L, o);
+			}
+			break;
+	}
+	oid = syck_add_sym(p, (char *)o);
+	return oid;
+}
+
+void lua_syck_emitter_handler(SyckEmitter *e, st_data_t data)
+{
+	struct emitter_xtra *bonus = (struct emitter_xtra *)e->bonus;
+	int type = lua_type(bonus->L, data);
+	char buf[30];		/* we're asking for disaster here */
+	int i;
+	
+	switch (type)
+	{
+		case LUA_TBOOLEAN:
+			if (lua_toboolean(bonus->L, data))
+				strcpy(buf, "true");
+			else
+				strcpy(buf, "false");
+			syck_emit_scalar(e, NULL, scalar_none, 0, 0, 0, (char *)buf, strlen(buf));
+			break;
+		case LUA_TSTRING:
+			syck_emit_scalar(e, NULL, scalar_none, 0, 0, 0, (char *)lua_tostring(bonus->L, data), lua_strlen(bonus->L, data));
+			break;
+		case LUA_TNUMBER:
+			/* should handle floats as well */
+			//snprintf(buf, sizeof(buf), "%i", (int)lua_tonumber(bonus->L, 1));
+			snprintf(buf, sizeof(buf), "%i", (int)lua_tonumber(bonus->L, data));
+			syck_emit_scalar(e, NULL, scalar_none, 0, 0, 0, buf, strlen(buf));
+			break;
+		case LUA_TTABLE:
+			syck_emit_seq(e, NULL, seq_none);
+			lua_pushnil(bonus->L);  /* first key */
+			i=1;
+			while (lua_next(bonus->L, 1) != 0) {
+				/* `key' is at index -2 and `value' at index -1 */
+				lua_pushvalue(bonus->L, -1);
+				syck_emit_item(e, -2);
+				lua_pop(bonus->L, 1);  /* removes `value'; keeps `key' for next iteration */
+				i++;
+			}
+			syck_emit_end(e);
+			break;
+	}
+
+	/* do we need this */
+	lua_pop(bonus->L, 1);
+}
+
+static void lua_syck_mark_emitter(SyckEmitter *e, int idx)
+{
+	struct emitter_xtra *bonus = (struct emitter_xtra *)e->bonus;
+	int type = lua_type(bonus->L, idx);
+
+	switch (type) {
+		case LUA_TTABLE:
+			lua_pushnil(bonus->L);  /* first key */
+			while (lua_next(bonus->L, idx) != 0) {
+				/* `key' is at index -2 and `value' at index -1 */
+				syck_emitter_mark_node(e, -1);
+				lua_pop(bonus->L, 1);
+			}
+			break;
+		default:
+			syck_emitter_mark_node(e, idx);
+			break;
+	}
+}
+
+
+void lua_syck_output_handler(SyckEmitter *e, char *str, long len)
+{
+	struct emitter_xtra *bonus = (struct emitter_xtra *)e->bonus;
+	luaL_addlstring(&bonus->output, str, len);
+}
+
+static int syck_load(lua_State *L)
+{
+	struct parser_xtra *bonus;
+	SyckParser *parser;
+	SYMID v;
+	int obj;
+
+	if (!luaL_checkstring(L, 1))
+		luaL_typerror(L, 1, "string");
+
+	parser = syck_new_parser();
+	parser->bonus = S_ALLOC_N(struct emitter_xtra, 1);
+
+	bonus = (struct parser_xtra *)parser->bonus;
+	bonus->L = lua_newthread(L);
+
+	syck_parser_str(parser, (char *)lua_tostring(L, 1), lua_strlen(L, 1), NULL);
+	syck_parser_handler(parser, lua_syck_parser_handler);
+	v = syck_parse(parser);
+	syck_lookup_sym(parser, v, (char **)&obj);
+
+	syck_free_parser(parser);
+
+	lua_xmove(bonus->L, L, 1);
+
+	return 1;
+}
+
+static int syck_dump(lua_State *L)
+{
+	SyckEmitter *emitter;
+	struct emitter_xtra *bonus;
+
+	emitter = syck_new_emitter();
+	emitter->bonus = S_ALLOC_N(struct emitter_xtra, 1);
+
+	bonus = (struct emitter_xtra *)emitter->bonus;
+	bonus->L = lua_newthread(L);
+	luaL_buffinit(L, &bonus->output);
+
+	syck_emitter_handler(emitter, lua_syck_emitter_handler);
+	syck_output_handler(emitter, lua_syck_output_handler);
+
+	lua_pushvalue(L, -2);
+	lua_xmove(L, bonus->L, 1);
+
+	lua_syck_mark_emitter(emitter, lua_gettop(bonus->L));
+	syck_emit(emitter, lua_gettop(bonus->L));
+	syck_emitter_flush(emitter, 0);
+
+	luaL_pushresult(&bonus->output);
+	syck_free_emitter(emitter);
+
+	return 1;
+}
+
+static const luaL_reg sycklib[] = {
+	{"load",	syck_load },
+	{"dump",	syck_dump },
+	{NULL, NULL}
+};
+
+LUALIB_API int luaopen_syck(lua_State *L)
+{
+	luaL_openlib(L, "syck", sycklib, 0);
+	return 1;
+}
---
+++ ext/lua/test.lua
@@ -0,0 +1,53 @@
+
+require "lunit"
+
+lunit.import "all"
+
+assert(loadlib("./lsyck.so", "luaopen_syck"))()
+
+local testcase = lunit.TestCase("LuaYAML Testcases")
+
+function testcase.test_load()
+	assert_error(function() syck.load() end)
+	assert_nil(syck.load("--- "))
+	assert_true(syck.load("--- true"))
+	assert_false(syck.load("--- false"))
+	assert_equal(10, syck.load("--- 10"))
+	local t = syck.load("--- \n- 5\n- 10\n- 15")
+	assert_equal(5, t[1])
+	assert_equal(10, t[2])
+	assert_equal(15, t[3])
+	local t = syck.load("--- \n- one\n- two\n- three")
+	assert_equal("one", t[1])
+	assert_equal("two", t[2])
+	assert_equal("three", t[3])
+	local t = syck.load("--- \nthree: 15\ntwo: 10\none: 5")
+	assert_equal(5, t.one)
+	assert_equal(10, t.two)
+	assert_equal(15, t.three)
+	local t = syck.load("--- \nints: \n  - 1\n  - 2\n  - 3\nmaps: \n  three: 3\n  two: 2\n  one: 1\nstrings: \n  - one\n  - two\n  - three")
+	assert_equal(1, t.ints[1])
+	assert_equal(2, t.ints[2])
+	assert_equal(3, t.ints[3])
+	assert_equal(1, t.maps.one)
+	assert_equal(2, t.maps.two)
+	assert_equal(3, t.maps.three)
+	assert_equal("one", t.strings[1])
+	assert_equal("two", t.strings[2])
+	assert_equal("three", t.strings[3])
+end
+
+function testcase.test_dump()
+	--assert_equal("--- ", syck.dump(nil))
+	assert_equal("--- hey\n", syck.dump("hey"))
+	assert_equal("--- 5\n", syck.dump(5))
+	assert_equal("--- true\n", syck.dump(true))
+	assert_equal("--- false\n", syck.dump(false))
+	assert_equal("--- \n- 1\n- 2\n- 3\n", syck.dump({1, 2, 3}))
+
+	-- currently segfaults
+	--local str = "--- \n- \n  - 1\n  - 2\n  - 3\n- \n  - one\n  - two\n  - three"
+	--assert_equal(str, syck.dump({{1, 2, 3}, {"one", "two", "three"}}))
+end
+
+os.exit(lunit.run())
