6f87c2ff06dafe5946607bf306e814e75cdf0bb0
Alexey Zakhlestin <indeyets@gmail.com>
2008-04-27 13:38:40 +0000

    test for long strings

--- ext/php/phpunit-tests/TestDump.php
+++ ext/php/phpunit-tests/TestDump.php
@@ -74,6 +74,12 @@ class TestDump extends PHPUnit_Framework_TestCase
         $this->assertSame($obj, syck_load(syck_dump($obj)));
     }
 
+    public function testLongStrings()
+    {
+        $obj = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.\nDuis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.\nExcepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+        $this->assertSame($obj, syck_load(syck_dump($obj)));
+    }
+
     public function testLargeArrays()
     {
         $obj = range(1, 10000);
